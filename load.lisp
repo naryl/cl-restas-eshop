866537991b1b362201e53478be57256a38f7d399
Wolfor <wolforus@gmail.com>
2013-08-13 17:16:56 +0400

    new swank version

--- load.lisp
+++ load.lisp
@@ -1,12 +1,30 @@
+(load "~/app/sbcl-1.1.10/contrib/asdf/asdf.lisp")
 (require 'asdf)
+
+(defvar *subdir-search-registry* '(#P"~/eshop/aux-libs/")
+  "List of directories to search subdirectories within.")
+(defvar *subdir-search-wildcard* :wild-inferiors
+  "Value of :wild means search only one level of subdirectories; value of :wild-inferiors means search all levels of subdirectories (I don't advise using this in big directories!)")
+(defun sysdef-subdir-search (system)
+  (let ((latter-path (make-pathname :name (asdf:coerce-name system)
+                                    :directory (list :relative
+                                                     *subdir-search-wildcard*)
+                                    :type "asd"
+                                    :version :newest
+                                    :case :local)))
+    (dolist (d *subdir-search-registry*)
+      (let* ((wild-path (merge-pathnames latter-path d))
+             (files (directory wild-path)))
+        (when files
+          (return (first files)))))))
 
 (defun load.register-libs ()
   (progn
+    (pushnew 'sysdef-subdir-search asdf:*system-definition-search-functions*)
     (mapcar #'(lambda (lib-name)
                 (push (truename (merge-pathnames lib-name *path-to-libs*)) asdf:*central-registry*))
             (list
-             ;; "slime-2011-09-01" ;; актуальный swank
-             "slime-archimag" ;; SWANK
+             "slime" ;; SWANK
              ;; WEB Server
              "hunchentoot" ;; HUNCH restas
              "cl-fad" ;; hunch (test ok)
@@ -32,6 +50,7 @@
              "split-sequence" ;; cl-routes
              "iterate" ;; cl-routes
              "data-sift" ;; restas
+             "puri-unicode" ;; data-sift
              "parse-number" ;; data-sift
 					   "closer-mop" ;;restas
              ;; CLOP (ESHOP)
@@ -49,18 +68,19 @@
              "cl-unicode" ;; cl-csv
              "cl-mime" ;; eshop
              "cl-qprint" ;; cl-mime
+             "cl-heap" ;; heap and priority queue
 
              ;; WEB Server test
-             "cl-who" ;; hunch-test
-             "drakma" ;; hunch-test http клиент для тестов
-             "puri-unicode" ;; drakma
-             "Eos" ;; esrap test
-             "cl-heap" ;; heap and priority queue
+             ;; "cl-who" ;; hunch-test
+             ;; "drakma" ;; hunch-test http клиент для тестов
+             ;; "puri-unicode" ;; drakma
+             ;; "Eos" ;; esrap test
+
 
            ;; ;;mongoDB
            ;; "cl-mongo"
            ;; "lisp-unit" ;; mongo
-           ;; "documentation-template-0.4.2" ;; mongo
+           ;; "documentation-template" ;; mongo
            ;; "uuid" ;; mongo
            ;; "trivial-utf-8" ;; mongo
            ;; "ironclad" ;; mongo
