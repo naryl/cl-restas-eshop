52f608d19076e6bf25ea721f3a64809772a8ef01
Wolfor <wolforus@gmail.com>
2013-08-14 18:07:53 +0400

    modify configs & default start script

--- bin/eshop.sh
+++ bin/eshop.sh
@@ -1,6 +1,6 @@
 ESHOP_PATH="$HOME/cl-restas-eshop/"
-CONFIG_PATH=$ESHOP_PATH"example-start/config-tosha.conf"
-LIBS_PATH=$ESHOP_HOME"libs/"
+CONFIG_PATH=$ESHOP_PATH"config/default.conf"
+LIBS_PATH="$HOME/cl-libs/"
 SWANK_PORT=4005
 
 export ESHOP_PATH
