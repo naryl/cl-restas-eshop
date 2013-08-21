4cc06516fa3600f22f1586a315fff142e119eea3
wolfor <wolforus@gmail.com>
2013-08-21 17:37:41 +0400

    start eshop using screen

--- bin/eshop.sh
+++ bin/eshop.sh
@@ -8,4 +8,4 @@ export CONFIG_PATH
 export LIBS_PATH
 export SWANK_PORT
 
-sbcl --lose-on-corruption --disable-ldb --dynamic-space-size 1024 --load $ESHOP_PATH"start-eshop.lisp"
+screen -dmS eshop sbcl --lose-on-corruption --disable-ldb --dynamic-space-size 2048 --load $ESHOP_PATH"start-eshop.lisp"
