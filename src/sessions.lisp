8346dbba95e6c472b67e6b744712b248d6434684
Alexander Suhoverhov <cy@ngs.ru>
2013-09-17 18:40:55 +0400

    Unversioning session:cart. Too much data to store

--- src/sessions.lisp
+++ src/sessions.lisp
@@ -22,7 +22,6 @@ was sent when this session was created.")
 when this session was started as returned by REAL-REMOTE-ADDR.")
    (cart :initarg :cart
          :serializable t
-         :versioned t
          :type string)
    (nc-user :initarg :nc-user
             :serializable t
