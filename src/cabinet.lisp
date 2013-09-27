f4af12b718575e023b7ae670fb0af8cf63869541
wolfor <wolforus@gmail.com>
2013-09-27 13:05:21 +0400

    fix decorators list

--- src/cabinet.lisp
+++ src/cabinet.lisp
@@ -9,10 +9,10 @@
 
 
 (restas:define-route request-user-orders-route ("/user/orders")
-  (:decorators '(@timer @session @no-cache))
+  (:decorators '@timer '@session '@no-cache)
   (soy.cabinet:main))
 
 
 (restas:define-route request-user-profile-route ("/user/profile")
-  (:decorators '(@timer @session @no-cache))
+  (:decorators '@timer '@session '@no-cache)
   (soy.cabinet:main))
