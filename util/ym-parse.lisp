459d305ec0495d5472b05f38a2a40df4adfcf0f5
Alexander Suhoverhov <cy@ngs.ru>
2013-10-30 15:43:02 +0400

    Yandex Market image parser

---
+++ util/ym-parse.lisp
@@ -0,0 +1,78 @@
+
+(defpackage eshop.parse-ym
+  (:use :cl
+        :alexandria
+        :drakma))
+
+(in-package eshop.parse-ym)
+
+(defun get-product-page (product)
+  (cl-html-parse:parse-html
+   (drakma:http-request
+    (format nil "http://market.yandex.ru/search.xml?text=~A&cvredirect=2"
+            (hunchentoot:url-encode product))
+    :preserve-uri t)))
+
+(defun get-product-image-url (product)
+  (let ((img-span (query (get-product-page product) ".b-model-pictures__big")))
+    (or (tag-attr (query img-span "a") :href)
+        (tag-attr (query img-span "img") :src))))
+
+;;;; JQUERY
+
+(defun tag-attrs (tag)
+  (rest (ensure-list (first tag))))
+
+(defun tag-attr (tag attr)
+  (getf (tag-attrs tag) attr))
+
+(defun query (src-html query)
+  (let ((tokens (split-sequence:split-sequence #\Space query)))
+    (loop :for token :in (list* nil tokens)
+       :for html = src-html :then (query-one html (parse-selector token))
+       :finally (return html))))
+
+(defun query-one (html selector)
+  (walk html #'(lambda (tag)
+                 (apply-selector tag selector))))
+
+(defun parse-selector (selector)
+  (multiple-value-bind (match tokens)
+      (cl-ppcre:scan-to-strings "^([a-zA-Z-_]*)(([#.])([a-zA-Z-_]*))?$" selector)
+    (declare (ignore match))
+    (let ((tag (elt tokens 0))
+          (type (elt tokens 2))
+          (value (elt tokens 3)))
+      (list :tag (when (string/= "" tag)
+                   (make-keyword (string-upcase tag)))
+            :type (when type
+                    (ecase (elt type 0)
+                      (#\# :id)
+                      (#\. :class)))
+            :value value))))
+
+(defun apply-selector (element selector)
+  (labels ((select (&key tag type value)
+             (when (or (not tag)
+                       (eq tag (first element)))
+               (when (or (and (not type)
+                              (not value))
+                         (equal value (getf (cdr element) type)))
+                 t))))
+    (apply #'select selector)))
+
+(defun walk (html matcher)
+  (labels ((walk (html)
+             (if (funcall matcher (ensure-list (first html)))
+                 html
+                 (some #'walk (remove-if-not #'listp (rest html))))))
+    (if (eq (caar html) :!doctype)
+        (walk (second html))
+        (walk html))))
+
+(defun xpath (html &rest route)
+  (let ((child (assoc (first route) (rest html)
+                      :key #'(lambda (item) (if (listp item) (first item) item)))))
+    (if (rest route)
+        (apply #'xpath child (rest route))
+        child)))
